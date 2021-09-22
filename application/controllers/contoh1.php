412d688430a4fc4995465d1c1c6a401991ab1aa3
LailiAlifah <lailinurulalifah212@gmail.com>
2021-09-22 21:02:30 +0700

    upload awal project

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index ()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Laili Nurul Alifah <br>"; 
+        echo "Saya tinggal di daerah Slawi <br>";
+        echo "hobi saya adalah merawat tanaman.";
+    }
+}
