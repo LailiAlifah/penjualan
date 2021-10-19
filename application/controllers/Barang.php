7f22b990178a1f42d617ad0e1d0de24c600ce3e6
LailiAlifah <lailinurulalifah212@gmail.com>
2021-10-19 11:08:00 +0700

    LailiAlifah

--- application/controllers/Barang.php
+++ application/controllers/Barang.php
@@ -10,7 +10,7 @@ class Barang extends CI_controller{
         $this->load->library('form_validation');
     }
     
-    function index()
+    public function index()
     {
         $data['judul']='Barang';
         $data['barang']=$this->Barang_model->getAllBarang();
@@ -19,9 +19,10 @@ class Barang extends CI_controller{
         $this->load->view('templates/footer');
     }
 
-    function tambah()
+    public function tambah()
     {
         $data['judul']="Tambah Data Barang";
+
         $this->form_validation->set_rules('id_barang', 'Kode Barang', 'required');
         $this->form_validation->set_rules('nama_barang', 'Nama Barang', 'required');
         $this->form_validation->set_rules('harga', 'harga', 'required|numeric');
@@ -35,9 +36,47 @@ class Barang extends CI_controller{
                 else
                 {
                     $this->Barang_model->tambahDataBarang();
+                    $this->session->set_flashdata('flash','ditambahkan');
                     redirect('barang');
                 }
        
+    }
+
+    public function hapus($id){
+        $this->Barang_model->hapusDataBarang($id);
+        $this->session->set_flashdata('flash','Dihapus');
+        redirect('barang');
+
+    }
+
+    public function detail($id){
+        $data['judul']='Detail Barang';
+        $data['barang']=$this->Barang_model->getBarangById($id);
+        $this->load->view('templates/header', $data);
+        $this->load->view('barang/detail', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function ubah($id){
+        $data['judul']="Ubah Data Barang";
+        $data['barang']=$this->Barang_model->getBarangById($id);
+    
+        $this->form_validation->set_rules('id_barang', 'Kode Barang', 'required');
+        $this->form_validation->set_rules('nama_barang', 'Nama Barang', 'required');
+        $this->form_validation->set_rules('harga', 'harga', 'required|numeric');
+        $this->form_validation->set_rules('stok', 'stok', 'required|numeric');
+        if ($this->form_validation->run() == FALSE)
+                {
+                    $this->load->view('templates/header', $data);
+                    $this->load->view('barang/ubah', $data);
+                    $this->load->view('templates/footer');
+                }
+                else
+                {
+                    $this->Barang_model->ubahDataBarang();
+                    $this->session->set_flashdata('flash','diubah');
+                    redirect('barang');
+                }
     }
 
 }
--- application/views/barang/index.php
+++ application/views/barang/index.php
@@ -1,4 +1,11 @@
 <div class="container">
+<?php if ($this->session->flashdata('flash')): ?>
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+  <strong>Data Barang</strong> <?= $this->session->flashdata('flash'); ?>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div
+<?php endif; ?>
+
   <div class="row-mt-3">
     <div class="colmd-6"></div>
       <a href="" class="btn btn-primary">Tambah Data Barang</a>
@@ -9,18 +16,25 @@
       <th scope="col">Nama Barang </th>
       <th scope="col">Harga</th>
       <th scope="col">Stok</th>
+      <th scope="col">Action</th>
     </tr>
   </thead>
   <tbody>
     <tr>
     <?php foreach ($barang as $brg):?>
-      
       <td><?= $brg['id_barang'];?></td>
       <td><?= $brg['nama_barang'];?></td>
       <td><?= $brg['harga'];?></td>
       <td><?= $brg['stok'];?></td>
-      </tr>
+
+      <td><a href="<?= base_url(); ?>barang/detail/<?=$brg['id_barang'];?>" class="btn btn-primary"
+      >Detail</a>
+      <a href="<?= base_url();?>barang/ubah/<?=$brg['id_barang'];?>" class="btn btn-success">Ubah</a>
+      <a href="<?= base_url();?>barang/hapus/<?=$brg['id_barang'];?>" class="btn btn-danger" onclick="return confirm ('Apakah Yakin Akan Dihapus?')">Hapus</a></td>  
+    </tr>
     <?php endforeach ?>
+
+
 
    
   </tbody>
